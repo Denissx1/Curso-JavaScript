c86097a52255c56abbdddcf02a86baa0244a60f2
Deniss Rangel <rangeldenison@gmail.com>
2024-05-24 13:46:23 -0400

    Modificado juego.js

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -3,6 +3,8 @@
 let deck         = [];
 const tipos      = ['C','D','H','S'];
 const especiales = ['A','J','Q','K'];
+
+// Esta funcion crea una nueva funcion
 const crearDeck = () => {
  for(let i = 2; i<=10; i++){
     for(let tipo of tipos){
@@ -16,10 +18,32 @@ const crearDeck = () => {
    }
  }
 
- console.log(deck);
+ //console.log(deck);
   deck = _.shuffle(deck);
   console.log(deck);
   return deck;
 }
 
 crearDeck()
+// Esta funcion me permite crear una carta
+const pedirCarta = () =>{
+if(deck.length === 0){
+throw 'no hay cartas en el deck';
+
+}
+const carta = deck.pop();
+console.log(deck);
+console.log(carta);
+    return carta;
+}
+ //   pedirCarta();
+const valorCarta = (carta) =>{
+
+ const valor = carta.substring(0, carta.length - 1);
+  return (isNaN(valor))   ?
+          (valor === 'A') ? 11 : 10
+          : valor * 1;
+ 
+}
+const valor = valorCarta(pedirCarta());
+console.log({valor});
